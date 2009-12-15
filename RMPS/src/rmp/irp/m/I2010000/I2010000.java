4e8aaf4115a97e5ef52574507e0eb324f9c8fe17
amonsoft <amonsoft@27759140-d95f-11de-8d45-899910d1343a>
2009-12-15 05:43:22 +0000

    IRP 服务命名规范化。

---
+++ RMPS/src/rmp/irp/m/I2010000/I2010000.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmp.irp.m.I2010000;
+
+/**
+ * 身份查询
+ * @author Amon
+ */
+public class I2010000
+{
+}
