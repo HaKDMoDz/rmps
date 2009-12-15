baf310805f68b0c53fe5914febb6ca9848f10bbe
amonsoft <amonsoft@27759140-d95f-11de-8d45-899910d1343a>
2009-12-15 16:47:32 +0000

    共用模块代码路径调整。

--- RMPS/src/rmp/irp/m/I2060000/I2060000.java
+++ RMPS/src/rmp/irp/m/I2060000/I2060000.java
@@ -7,6 +7,10 @@
  */
 package rmp.irp.m.I2060000;
 
+import com.amonsoft.rmps.irp.b.IMessage;
+import com.amonsoft.rmps.irp.b.ISession;
+import com.amonsoft.rmps.irp.m.IService;
+
 /**
  * <ul>
  * <li>功能说明：</li>
@@ -17,6 +21,47 @@ package rmp.irp.m.I2060000;
  * </ul>
  * @author Amon
  */
-public class I2060000 {
+public class I2060000 implements IService
+{
+    @Override
+    public boolean wInit()
+    {
+        return true;
+    }
 
+    @Override
+    public String getCode()
+    {
+        return "52060000";
+    }
+
+    @Override
+    public String getName()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getDescription()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doInit(ISession session, IMessage message)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doHelp(ISession session, IMessage message)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doDeal(ISession session, IMessage message)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
