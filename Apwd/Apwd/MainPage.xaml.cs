ec9312f77ac17e176b39b74a0b821f098687b679
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-01-04 14:02:25 +0000

    《阿木密码箱》项目初始化

--- Apwd/Apwd/MainPage.xaml.cs
+++ Apwd/Apwd/MainPage.xaml.cs
@@ -1,22 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using Me.Amon.Apwd.Utils;
+using Me.Amon.Apwd.Views;
+using Me.Amon.Apwd.Views.Demo;
+using Me.Amon.Apwd.Views.Home;
+using Me.Amon.Apwd.Views.Mopt;
 
 namespace Me.Amon.Apwd
 {
     public partial class MainPage : UserControl
     {
+        private IView _View;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void LayoutRoot_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _View = new Home();
+            _View.InitView(this);
+            _View.InitData();
+            BeanUtil.Loading = Loading;
+        }
+
+        public void Show(UserControl control)
+        {
+            Body.Children.Clear();
+            Body.Children.Add(control);
+        }
+
+        public void ShowUser()
+        {
+            DvUser.Visibility = System.Windows.Visibility.Visible;
+            HbUser.Visibility = System.Windows.Visibility.Visible;
+
+            DvOpts.Visibility = System.Windows.Visibility.Visible;
+            HbOpts.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        public void HideUser()
+        {
+            DvUser.Visibility = System.Windows.Visibility.Collapsed;
+            HbUser.Visibility = System.Windows.Visibility.Collapsed;
+
+            DvOpts.Visibility = System.Windows.Visibility.Collapsed;
+            HbOpts.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void HbUser_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _View = new Home();
+            _View.InitView(this);
+            _View.InitData();
+            DvOpts.Visibility = System.Windows.Visibility.Collapsed;
+            HbOpts.Visibility = System.Windows.Visibility.Collapsed;
+            DvUser.Visibility = System.Windows.Visibility.Collapsed;
+            HbUser.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void HbOpts_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_View.ViewName != "Mopt")
+            {
+                _View = new Mopt();
+                _View.InitView(this);
+                _View.InitData();
+            }
+        }
+
+        private void HbDemo_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (_View.ViewName != "Demo")
+            {
+                _View = new Demo();
+                _View.InitView(this);
+                _View.InitData();
+            }
+            else
+            {
+                _View = new Home();
+                _View.InitView(this);
+                _View.InitData();
+            }
         }
     }
 }
