1bba89f86df494587f19974aae11bd0a5e0eb251
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-01-04 14:15:50 +0000

    《阿木密码箱》项目初始化

---
+++ Apwd/Apwd/Views/Demo/Demo.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Controls;
+using Me.Amon.Apwd.Views.Mpwd.Mpro;
+
+namespace Me.Amon.Apwd.Views.Demo
+{
+    public partial class Demo : UserControl, IView
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+
+        public string ViewName
+        {
+            get { return "Demo"; }
+        }
+
+        public bool InitView(MainPage main)
+        {
+            main.Show(this);
+            return true;
+        }
+
+        public void InitData()
+        {
+        }
+
+        private int last;
+        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (last == 0)
+            {
+                ShowSc2();
+                last = 1;
+            }
+            else
+            {
+                ShowSc1();
+                last = 0;
+            }
+        }
+
+        private void ShowSc1()
+        {
+        }
+
+        private void ShowSc2()
+        {
+        }
+    }
+}
