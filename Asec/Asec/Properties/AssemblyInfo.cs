9c980957f754d316965c796edf9ffc57e40161c2
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-01-06 13:32:14 +0000

    《阿木加密器》V1.0.0.0

--- Asec/Asec/Properties/AssemblyInfo.cs
+++ Asec/Asec/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amon.Me")]
 [assembly: AssemblyProduct("阿木加密器")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Amon 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
