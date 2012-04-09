32aec791b4069b93730409280b90d0d2cff3ec2a
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-04-09 13:20:04 +0000

    阿木密码箱 V6.0.0.1

---
+++ App/Lib/Da/DBA.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using Me.Amon.Model;
+using Me.Amon.Pwd;
+using Me.Amon.Ren;
+
+namespace Me.Amon.Da
+{
+    public interface DBA
+    {
+        void CloseConnect();
+
+        #region 数据更新
+        /// <summary>
+        /// 存储数据
+        /// </summary>
+        /// <param name="vcs"></param>
+        void SaveVcs(Vcs vcs);
+
+        /// <summary>
+        /// 存储日志
+        /// </summary>
+        /// <param name="log"></param>
+        void SaveLog(Log log);
+        #endregion
+
+        #region 数据删除
+        /// <summary>
+        /// 逻辑移除
+        /// </summary>
+        /// <param name="vcs"></param>
+        void RemoveVcs(Vcs vcs);
+
+        /// <summary>
+        /// 物理删除
+        /// </summary>
+        /// <param name="vcs"></param>
+        void DeleteVcs(Vcs vcs);
+
+        void DeleteLog(Log log);
+        #endregion
+
+        #region 字符操作
+        IList<Udc> ListUdc();
+        #endregion
+
+        #region 类别操作
+        Cat ReadCat(string catId);
+
+        IList<Cat> FindCat(string catMeta);
+
+        IList<Cat> ListCat(string parentId);
+        #endregion
+
+        #region 记录操作
+        Key ReadKey(string keyId);
+
+        IList<Key> FindKey(string keyMeta);
+
+        IList<Key> ListKey(string catId);
+
+        IList<Key> FindKeyByLabel(int label);
+
+        IList<Key> FindKeyByMajor(int major);
+        #endregion
+
+        #region 日志操作
+        KeyLog ReadKeyLog(string logId);
+
+        IList<KeyLog> ListKeyLog(string keyId);
+        #endregion
+
+        #region 模板操作
+        IList<Lib> ListLib();
+        #endregion
+
+        #region 目录操作
+        IList<Dir> ListDir();
+        #endregion
+
+        IList<MRen> ListRen();
+    }
+}
---
+++ App/Lib/Pwd/Udc.cs
@@ -0,0 +1,90 @@
+﻿using System.Xml;
+using Me.Amon.Model;
+
+namespace Me.Amon.Pwd
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Udc : Vcs
+    {
+        public int Order { get; set; }
+        public string Name { get; set; }
+        public string Tips { get; set; }
+        public string Data { get; set; }
+        public string Memo { get; set; }
+
+        #region 方法重写
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Udc)
+            {
+                return Id == ((Udc)obj).Id;
+            }
+            if (obj is string)
+            {
+                return Id == (string)obj;
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id != null ? Id.GetHashCode() : 0;
+        }
+        #endregion
+
+        public bool FromXml(XmlReader reader)
+        {
+            if (reader == null || reader.Name != "Udc")
+            {
+                return false;
+            }
+
+            if (reader.Name == "Order" || reader.ReadToDescendant("Order"))
+            {
+                Order = reader.ReadElementContentAsInt();
+            }
+            if (reader.Name == "Id" || reader.ReadToNextSibling("Id"))
+            {
+                Id = reader.ReadElementContentAsString();
+            }
+            if (reader.Name == "Name" || reader.ReadToNextSibling("Name"))
+            {
+                Name = reader.ReadElementContentAsString();
+            }
+            if (reader.Name == "Tips" || reader.ReadToNextSibling("Tips"))
+            {
+                Tips = reader.ReadElementContentAsString();
+            }
+            if (reader.Name == "Data" || reader.ReadToNextSibling("Data"))
+            {
+                Data = reader.ReadElementContentAsString();
+            }
+            if (reader.Name == "Memo" || reader.ReadToNextSibling("Memo"))
+            {
+                Data = reader.ReadElementContentAsString();
+            }
+            return true;
+        }
+
+        public void ToXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("Udc");
+
+            writer.WriteElementString("Order", Order.ToString());
+            writer.WriteElementString("Id", Id);
+            writer.WriteElementString("Name", Name);
+            writer.WriteElementString("Tips", Tips);
+            writer.WriteElementString("Data", Data);
+            writer.WriteElementString("Memo", Memo);
+
+            writer.WriteEndElement();
+        }
+    }
+}
