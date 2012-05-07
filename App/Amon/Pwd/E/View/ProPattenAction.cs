283a05b96b40ad6489887274630f4e3c9fa62780
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-05-07 01:31:57 +0000

    菜单选项组问题。

--- App/Amon/Pwd/E/View/ProPattenAction.cs
+++ App/Amon/Pwd/E/View/ProPattenAction.cs
@@ -1,4 +1,4 @@
-﻿namespace Me.Amon.Pwd.E.View
+ namespace Me.Amon.Pwd.E.View
 {
     public class ProPattenAction : APwdAction
     {
@@ -7,6 +7,44 @@
             if (IApp != null)
             {
                 IApp.ShowAPro();
+            }
+            
+            string cmd;
+            if (sender is ToolStripItem)
+            {
+                ToolStripItem item = sender as ToolStripItem;
+                object obj = item.Tag;
+                if (obj == null || !(obj is string))
+                {
+                    return;
+                }
+
+                cmd = obj as string;
+            }
+            else if (sender is KeyStroke)
+            {
+                KeyStroke stroke = sender as KeyStroke;
+                cmd = stroke.Command;
+            }
+            else
+            {
+                return;
+            }
+
+            foreach (string tmp in cmd.Split(';'))
+            {
+                string[] arr = cmd.Split(':');
+                if (arr.Length != 2)
+                {
+                    continue;
+                }
+
+                ItemGroup group = IApp.GetGroup(arr[0]);
+                if (group == null)
+                {
+                    continue;
+                }
+                group.Checked(arr[1]);
             }
         }
     }
