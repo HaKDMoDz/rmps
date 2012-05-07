bc0e298c2c514ab5cd87bb46c4de8696b2807c8e
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-05-07 01:43:40 +0000

    视图模式。

--- App/Amon/Pwd/EPwd.cs
+++ App/Amon/Pwd/EPwd.cs
@@ -1,6 +1,11 @@
-﻿namespace Me.Amon.Pwd
+ namespace Me.Amon.Pwd
 {
     public class EPwd
     {
+        public const string PATTERN_PRO = "pro";
+
+        public const string PATTERN_WIZ = "wiz";
+
+        public const string PATTERN_PAD = "pad";
     }
 }
