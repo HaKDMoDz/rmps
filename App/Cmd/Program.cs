33423c451a1cf9c8de0c619960e276658198c788
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-06-09 01:59:26 +0000

    阿木密码箱 V7.1.2.8

--- App/Cmd/Program.cs
+++ App/Cmd/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.IO;
 
 namespace Cmd
 {
@@ -12,8 +6,33 @@ namespace Cmd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Keys.Add.ToString());
-            Console.Read();
+            string path = @"F:\App\Amon\bin\Debug\Dat\A0000000\KEY";
+            DFile(path);
+
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                DFile(dir);
+            }
+        }
+
+        private static void DFile(string dir)
+        {
+            foreach (string file in Directory.GetFiles(dir))
+            {
+                string src = Path.GetFileName(file);
+                string dst = Path.GetFileNameWithoutExtension(file);
+                MoveTo(dir, src, dst);
+            }
+        }
+
+        private static void MoveTo(string path, string src, string dst)
+        {
+            src = path + '\\' + src;
+            dst = path + '\\' + dst;
+            if (File.Exists(src))
+            {
+                File.Move(src, dst);
+            }
         }
     }
 }
