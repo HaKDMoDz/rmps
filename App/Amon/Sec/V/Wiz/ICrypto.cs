56bbf2417fade2cb302602c35b7f9dfd95a4da09
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-06-10 06:19:50 +0000

    阿木密码箱 V7.1.4.9

---
+++ App/Amon/Sec/V/Wiz/ICrypto.cs
@@ -0,0 +1,9 @@
+﻿namespace Me.Amon.Sec.V.Wiz
+{
+    interface ICrypto
+    {
+        bool Encrypt(string src, string dst);
+
+        bool Decrypt(string src, string dst);
+    }
+}
--- App/Amon/Sec/V/Wiz/IView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace Me.Amon.Sec.V.Wiz
-{
-    interface IView
-    {
-        Control Control { get; }
-    }
-}
