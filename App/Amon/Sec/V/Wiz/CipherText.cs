4e15af6bf36a27a4ec55c0dc334044fd12615ece
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-06-12 09:20:16 +0000

    向导模式

--- App/Amon/Sec/V/Wiz/CipherText.cs
+++ App/Amon/Sec/V/Wiz/CipherText.cs
@@ -1,15 +1,113 @@
-﻿
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Me.Amon.Uc;
+using Me.Amon.Util;
+
 namespace Me.Amon.Sec.V.Wiz
 {
-    class CipherText : ICrypto
+    class Decode : ICrypto
     {
-        public bool Encrypt(string src, string dst)
+        private AFile _AFile;
+        private AText _AText;
+        public Decode(AFile file, AText text)
         {
+            _AFile = file;
+            _AText = text;
+        }
+
+        public void Init()
+        {
+            if (IsText)
+            {
+                return;
+            }
+
+            _AFile.ClSrc.HeaderText = "输入文件";
+            _AFile.ClDst.HeaderText = "输出文件";
+            _AFile.LlData.Text = "口令(&K)";
+        }
+
+        public bool IsText { get; set; }
+
+        public string Algorithm { get; set; }
+
+        public bool DoCrypto()
+        {
+            if (IsText)
+            {
+                return DecryptText();
+            }
+
+            if (_AFile.FileList == null || _AFile.FileList.Count < 1)
+            {
+                return false;
+            }
+            foreach (Item item in _AFile.FileList)
+            {
+                if (item.V.ToLower().EndsWith(".bin"))
+                {
+                    item.D = item.V.Substring(0, item.V.Length - 4);
+                }
+                return DecryptFile(Path.Combine(item.K, item.V), Path.Combine(item.K, item.D));
+            }
             return true;
         }
 
-        public bool Decrypt(string src, string dst)
+        private bool DecryptFile(string src, string dst)
         {
+            if (!File.Exists(src))
+            {
+                return false;
+            }
+
+            using (SymmetricAlgorithm alg = SymmetricAlgorithm.Create(Algorithm))
+            {
+                alg.Key = null;
+                alg.IV = null;
+
+                FileStream iStream = File.OpenRead(src);
+                FileStream oStream = File.OpenWrite(dst);
+                using (CryptoStream cStream = new CryptoStream(oStream, alg.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] buf = new byte[4096];
+                    int len = iStream.Read(buf, 0, buf.Length);
+                    while (len > 0)
+                    {
+                        cStream.Write(buf, 0, len);
+                    }
+                    cStream.FlushFinalBlock();
+                }
+                oStream.Close();
+                iStream.Close();
+            }
+            return true;
+        }
+
+        private bool DecryptText()
+        {
+            string src = _AText.TbSrc.Text;
+            if (!File.Exists(src))
+            {
+                return false;
+            }
+            using (SymmetricAlgorithm alg = SymmetricAlgorithm.Create(Algorithm))
+            {
+                alg.Key = null;
+                alg.IV = null;
+
+                byte[] buf;
+                MemoryStream mStream = new MemoryStream();
+                using (CryptoStream cStream = new CryptoStream(mStream, alg.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    buf = CharUtil.DecodeString(src);
+                    cStream.Write(buf, 0, buf.Length);
+                    cStream.FlushFinalBlock();
+                }
+                buf = mStream.ToArray();
+                _AText.TbDst.Text = Encoding.UTF8.GetString(buf);
+                mStream.Close();
+            }
             return true;
         }
     }
