86d447008b69974a90096aab46239a6f0cef9451
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-06-12 09:20:37 +0000

    向导模式

--- App/Amon/Sec/V/Wiz/DigestFile.cs
+++ App/Amon/Sec/V/Wiz/DigestFile.cs
@@ -1,15 +1,87 @@
-﻿
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Me.Amon.Uc;
+using Me.Amon.Util;
+
 namespace Me.Amon.Sec.V.Wiz
 {
-    class DigestFile : ICrypto
+    class Digest : ICrypto
     {
-        public bool Encrypt(string src, string dst)
+        private AFile _AFile;
+        private AText _AText;
+
+        public Digest(AFile file, AText text)
         {
+            _AFile = file;
+            _AText = text;
+        }
+
+        public void Init()
+        {
+            if (IsText)
+            {
+                return;
+            }
+
+            _AFile.ClSrc.HeaderText = "输入文件";
+            _AFile.ClDst.HeaderText = "输出文件";
+            _AFile.LlData.Text = "口令(&K)";
+        }
+
+        public bool IsText { get; set; }
+
+        public string Algorithm { get; set; }
+
+        public bool DoCrypto()
+        {
+            if (IsText)
+            {
+                return DigestText();
+            }
+
+            if (_AFile.FileList == null || _AFile.FileList.Count < 1)
+            {
+                return false;
+            }
+            foreach (Item item in _AFile.FileList)
+            {
+                DigestFile(item.K, item.D);
+            }
             return true;
         }
 
-        public bool Decrypt(string src, string dst)
+        private bool DigestFile(string src, string dst)
         {
+            if (!File.Exists(src))
+            {
+                return false;
+            }
+
+            using (HashAlgorithm alg = HashAlgorithm.Create(Algorithm))
+            {
+                FileStream stream = File.OpenRead(src);
+                byte[] buf = alg.ComputeHash(stream);
+                stream.Close();
+                dst = CharUtil.EncodeString(buf);
+            }
+            return true;
+        }
+
+        private bool DigestText()
+        {
+            string src = _AText.TbSrc.Text;
+            if (string.IsNullOrEmpty(src))
+            {
+                return false;
+            }
+
+            using (HashAlgorithm alg = HashAlgorithm.Create(Algorithm))
+            {
+                byte[] buf = Encoding.UTF8.GetBytes(src);
+                buf = alg.ComputeHash(buf);
+                _AText.TbDst.Text = CharUtil.EncodeString(buf);
+            }
             return true;
         }
     }
