122f450c3bbbc92f26443b0c64818c086619ec18
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-06-12 09:19:46 +0000

    向导模式

--- App/Amon/Sec/V/Wiz/CipherFile.cs
+++ App/Amon/Sec/V/Wiz/CipherFile.cs
@@ -1,16 +1,82 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Me.Amon.Uc;
+
 namespace Me.Amon.Sec.V.Wiz
 {
-    class CipherFile : ICrypto
+    public partial class AFile : UserControl
     {
-        public bool Encrypt(string src, string dst)
+        public List<Item> FileList = new List<Item>();
+
+        public AFile()
         {
-            return true;
+            InitializeComponent();
         }
 
-        public bool Decrypt(string src, string dst)
+        private void GvFile_DragDrop(object sender, DragEventArgs e)
         {
-            return true;
+            try
+            {
+                object obj = e.Data.GetData(DataFormats.FileDrop);
+                if (obj == null)
+                {
+                    return;
+                }
+                string[] arr = obj as string[];
+                if (arr == null)
+                {
+                    return;
+                }
+
+                foreach (string file in arr)
+                {
+                    if (!File.Exists(file))
+                    {
+                        continue;
+                    }
+
+                    if (GetD(file) != null)
+                    {
+                        continue;
+                    }
+
+                    Item item = new Item { K = file, V = Path.GetFileName(file) };
+                    FileList.Add(item);
+                    GvFile.Rows.Add(item.V, "");
+                }
+            }
+            catch (Exception ex)
+            {
+                Main.ShowError(ex);
+            }
         }
+
+        private void GvFile_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        #region 私有函数
+        private Item GetD(string file)
+        {
+            foreach (Item item in FileList)
+            {
+                if (item.K == file)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        #endregion
     }
 }
