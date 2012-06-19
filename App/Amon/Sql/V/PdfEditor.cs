15f5134567b394ba739679633157f47cf4a2a256
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-06-19 05:42:40 +0000

    布局调整。

--- App/Amon/Sql/V/PdfEditor.cs
+++ App/Amon/Sql/V/PdfEditor.cs
@@ -1,39 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using System.Xml;
+using Me.Amon.Model;
+using Me.Amon.Sql.C;
 using Me.Amon.Sql.Model;
 using Me.Amon.Sql.V.Pdq;
 
-namespace Me.Amon.Sql.Editor
+namespace Me.Amon.Sql.V
 {
     public partial class PdfEditor : UserControl, IEditor
     {
+        #region 全局变量
         private Dml _Sql;
-        private List<Label> _LlList;
-        private List<IInput> _UcList;
-        private List<RowStyle> _RsList;
         private RowStyle _DefStyle;
+        private List<IInput> _UcList;
+        private UserModel _UserModel;
+        #endregion
 
         #region 构造函数
         public PdfEditor()
         {
             InitializeComponent();
+        }
 
-            InitOnce();
+        public PdfEditor(UserModel userModel)
+        {
+            _UserModel = userModel;
+
+            InitializeComponent();
         }
         #endregion
 
-        public void InitOnce()
+        #region 接口实现
+        public void Init(IEngine engine)
         {
-            _LlList = new List<Label>();
-            _UcList = new List<IInput>();
-            _RsList = new List<RowStyle>();
             _DefStyle = new RowStyle(SizeType.Percent, 100F);
+            _UcList = new List<IInput>();
 
-            string file = "sql.xml";
+            string file = Path.Combine(_UserModel.Home, "ASql-Pdf.xml");
             if (!File.Exists(file))
             {
                 return;
@@ -50,7 +57,6 @@ namespace Me.Amon.Sql.Editor
             }
         }
 
-        #region 接口实现
         public bool Check()
         {
             if (_Sql == null || string.IsNullOrWhiteSpace(_Sql.Text))
@@ -70,14 +76,17 @@ namespace Me.Amon.Sql.Editor
         public string GetSQL()
         {
             string sql = _Sql.Text;
-            for (int i = 0; i < _Sql.Params.Count; i += 1)
+            Param param;
+            foreach (IInput input in _UcList)
             {
-                sql = sql.Replace('#' + _Sql.Params[i].Key + '#', _UcList[i].Value);
+                param = input.Param;
+                sql = sql.Replace('#' + param.Key + '#', param.Value);
             }
             return sql;
         }
         #endregion
 
+        #region 事件处理
         private void CbSqlList_SelectedIndexChanged(object sender, EventArgs e)
         {
             _Sql = CbSqlList.SelectedItem as Dml;
@@ -87,35 +96,43 @@ namespace Me.Amon.Sql.Editor
             }
 
             TbSql.Text = _Sql.Text;
-            TpInput.RowStyles.Clear();
             TpInput.Controls.Clear();
+            TpInput.RowStyles.Clear();
             _UcList.Clear();
 
-            int idx = 0;
+            foreach (string key in _ArgInput.Keys)
+            {
+                _ArgIndex[key] = 0;
+            }
+
+            int i = 0;
             int h = 6;
             Label label;
             IInput input;
             foreach (Param param in _Sql.Params)
             {
-                TpInput.RowStyles.Add(GetStyle(idx));
+                TpInput.RowStyles.Add(GetStyle(i));
 
-                label = GetLabel(idx);
-                TpInput.Controls.Add(label, 0, idx);
+                label = GetLabel(i);
+                TpInput.Controls.Add(label, 0, i);
                 label.Text = param.Text;
 
-                input = GetInput(idx, param.Type);
-                TpInput.Controls.Add(input.Control, 1, idx);
+                input = GetInput(param.Type);
+                TpInput.Controls.Add(input.Control, 1, i);
                 input.Param = param;
                 h += 27;
 
                 _UcList.Add(input);
-                idx += 1;
+                i += 1;
             }
 
             TpInput.RowStyles.Add(_DefStyle);
             TpInput.Height = h;
         }
+        #endregion
 
+        #region 私有函数
+        #region 动态组件
         private Label GetLabel(int idx)
         {
             Label label;
@@ -132,31 +149,59 @@ namespace Me.Amon.Sql.Editor
             }
             return label;
         }
+        private List<Label> _LlList = new List<Label>();
 
-        private IInput GetInput(int idx, string type)
+        private IInput GetInput(string type)
         {
-            IInput input;
-            switch (type)
+            List<IInput> list;
+            int idx;
+            if (_ArgInput.ContainsKey(type))
             {
-                case "text":
-                    input = new Text();
-                    break;
-                case "date":
-                    input = new Date();
-                    break;
-                case "data":
-                    input = new Data();
-                    break;
-                case "list":
-                    input = new List();
-                    break;
-                default:
-                    input = new None();
-                    break;
+                list = _ArgInput[type];
+                idx = _ArgIndex[type];
             }
+            else
+            {
+                list = new List<IInput>();
+                _ArgInput[type] = list;
+                idx = 0;
+                _ArgIndex[type] = idx;
+            }
+
+            IInput input;
+            if (idx < list.Count)
+            {
+                input = list[idx];
+            }
+            else
+            {
+                switch (type)
+                {
+                    case "text":
+                        input = new Text();
+                        break;
+                    case "date":
+                        input = new Date();
+                        break;
+                    case "data":
+                        input = new Data();
+                        break;
+                    case "list":
+                        input = new List();
+                        break;
+                    default:
+                        input = new None();
+                        break;
+                }
+                list.Add(input);
+            }
+            _ArgIndex[type] += 1;
+
             input.Control.Dock = DockStyle.Fill;
             return input;
         }
+        private Dictionary<string, List<IInput>> _ArgInput = new Dictionary<string, List<IInput>>();
+        private Dictionary<string, int> _ArgIndex = new Dictionary<string, int>();
 
         private RowStyle GetStyle(int idx)
         {
@@ -172,5 +217,8 @@ namespace Me.Amon.Sql.Editor
             }
             return style;
         }
+        private List<RowStyle> _RsList = new List<RowStyle>();
+        #endregion
+        #endregion
     }
 }
