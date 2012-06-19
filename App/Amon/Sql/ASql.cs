03dc680a729e913f92f7bd4d079281c50fec9642
Amon.WK@gmail.com <Amon.WK@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-06-19 04:59:35 +0000

    数据查询功能调整

--- App/Amon/Sql/ASql.cs
+++ App/Amon/Sql/ASql.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Data;
 using System.IO;
-using System.Reflection;
 using System.Windows.Forms;
 using System.Xml;
 using Me.Amon.Event;
 using Me.Amon.Model;
-using Me.Amon.Sql.Editor;
+using Me.Amon.Sql.C;
+using Me.Amon.Sql.C.SQLite;
 using Me.Amon.Sql.M;
-using Me.Amon.Sql.Model;
 using Me.Amon.Sql.V;
 using Me.Amon.Uc;
-using Me.Amon.Util;
 
 namespace Me.Amon.Sql
 {
@@ -20,15 +17,11 @@ namespace Me.Amon.Sql
     {
         #region 全局变量
         private Rdbms _Dbms;
-        private IEditor _Param;
-        private IDbConnection _Connect;
-        private IDbCommand _Command;
-        private IDbDataAdapter _Adapter;
-        private Dictionary<string, Assembly> _Assemblys;
-        private List<Rdbms> _Drives;
-        private DbResult _DataGrid;
+        private IEditor _DbEditor;
+        private IResult _DbResult;
         private XmlMenu<ASql> _XmlMenu;
         private UserModel _UserModel;
+        private IEngine _SqlEngine;
         #endregion
 
         #region 构造函数
@@ -64,7 +57,10 @@ namespace Me.Amon.Sql
 
         public bool WillExit()
         {
-            CloseConnection();
+            if (_SqlEngine != null)
+            {
+                _SqlEngine.Close();
+            }
             return true;
         }
 
@@ -78,50 +74,185 @@ namespace Me.Amon.Sql
         public void ChangeDdl()
         {
             DbConfig config = new DbConfig();
-            config.DriverList = _Drives;
+            //config.DriverList = _Drives;
             config.AmonHandler = new AmonHandler<Rdbms>(ChangeDdl);
             config.ShowDialog(this);
         }
 
         public void ChangeDdl(Rdbms ddl)
         {
-            CloseConnection();
+            if (_SqlEngine != null)
+            {
+                _SqlEngine.Close();
+            }
+            _SqlEngine = new SQLiteEngine();
+            _SqlEngine.Begin();
 
-            _Dbms = ddl;
-            GetConnection();
-            GetCommand();
+            NewPdqTab(true);
+            NewSqlTab(false);
+            NewDbrTab(true);
+        }
+
+        public TabPage NewPdqTab(bool selected)
+        {
+            int cnt = TcEditor.TabPages.Count;
+            TabPage page = new TabPage();
+            TcEditor.TabPages.Add(page);
+            //page.Location = new System.Drawing.Point(4, 23);
+            page.Name = "tabPage" + cnt;
+            page.Padding = new System.Windows.Forms.Padding(3);
+            //page.Size = new System.Drawing.Size(664, 179);
+            page.TabIndex = cnt;
+            page.Text = string.Format("预置查询 ({0})", cnt);
+            page.UseVisualStyleBackColor = true;
+
+            PdfEditor editor = new PdfEditor(_UserModel);
+            editor.Dock = DockStyle.Fill;
+            //editor.Location = new System.Drawing.Point(3, 3);
+            editor.Name = "UcSql";
+            //editor.Size = new System.Drawing.Size(658, 137);
+            editor.TabIndex = 0;
+            page.Controls.Add(editor);
+
+            editor.Init(_SqlEngine);
+
+            if (selected)
+            {
+                TcEditor.SelectedTab = page;
+                _DbEditor = editor;
+            }
+
+            return page;
+        }
+
+        public TabPage NewSqlTab(bool selected)
+        {
+            int cnt = TcEditor.TabPages.Count;
+            TabPage page = new TabPage();
+            TcEditor.TabPages.Add(page);
+            //page.Location = new System.Drawing.Point(4, 23);
+            page.Name = "tabPage" + cnt;
+            page.Padding = new System.Windows.Forms.Padding(3);
+            //page.Size = new System.Drawing.Size(664, 179);
+            page.TabIndex = cnt;
+            page.Text = string.Format("高级查询 ({0})", cnt);
+            page.UseVisualStyleBackColor = true;
+
+            SqlEditor editor = new SqlEditor(_UserModel);
+            editor.Dock = DockStyle.Fill;
+            //editor.Location = new System.Drawing.Point(3, 3);
+            editor.Name = "UcSql";
+            //editor.Size = new System.Drawing.Size(658, 137);
+            editor.TabIndex = 0;
+            page.Controls.Add(editor);
+
+            editor.Init(_SqlEngine);
+
+            if (selected)
+            {
+                TcEditor.SelectedTab = page;
+                _DbEditor = editor;
+            }
+
+            return page;
+        }
+
+        public TabPage NewDbrTab(bool selected)
+        {
+            int cnt = TcResult.TabPages.Count;
+            TabPage page = new TabPage();
+            TcResult.TabPages.Add(page);
+            //page.Location = new System.Drawing.Point(4, 23);
+            page.Name = "tabPage" + cnt;
+            page.Padding = new System.Windows.Forms.Padding(3);
+            //page.Size = new System.Drawing.Size(664, 179);
+            page.TabIndex = cnt;
+            page.Text = string.Format("结果 ({0})", cnt);
+            page.UseVisualStyleBackColor = true;
+
+            DbResult result = new DbResult();
+            page.Controls.Add(result);
+            result.Dock = DockStyle.Fill;
+            result.TabIndex = 0;
+
+            if (selected)
+            {
+                TcResult.SelectedTab = page;
+                _DbResult = result;
+            }
+
+            return page;
+        }
+
+        public void ImportCsv(string csvFile, CsvConfig csvCfg)
+        {
+            int cnt = _SqlEngine.ImportCsv(csvFile, csvCfg);
+            if (cnt > -1)
+            {
+                ShowEcho(string.Format("成功导入 {0} 条记录！", cnt));
+            }
+        }
+
+        public void ImportSql(string sqlFile, SqlConfig sqlCfg)
+        {
+            _SqlEngine.ImportSql(sqlFile, sqlCfg);
+        }
+
+        public void ImportXml(string xmlFile, XmlConfig xmlCfg)
+        {
+            _SqlEngine.ImportXml(xmlFile, xmlCfg);
+        }
+
+        public void ExportCsv(string csvFile, CsvConfig csvCfg)
+        {
+            _SqlEngine.ExportCsv(csvFile, csvCfg);
+        }
+
+        public void ExportSql(string sqlFile, SqlConfig sqlCfg)
+        {
+            _SqlEngine.ExportSql(sqlFile, sqlCfg);
+        }
+
+        public void ExportXml(string xmlFile, XmlConfig xmlCfg)
+        {
+            _SqlEngine.ExportXml(xmlFile, xmlCfg);
         }
         #endregion
 
         #region 事件处理
         private void ASql_Load(object sender, EventArgs e)
         {
+            KeyPreview = true;
+
             _XmlMenu = new XmlMenu<ASql>(this, null);
             if (_XmlMenu.Load(Path.Combine(_UserModel.Home, ESql.XML_MENU)))
             {
                 _XmlMenu.GetStrokes("ASql");
+                _XmlMenu.GetPopMenu("ASql", CmMenu);
             }
 
-            string path = Path.Combine(_UserModel.Home, ESql.DDL_FILE);
-            if (!File.Exists(path))
-            {
-                return;
-            }
+            //string path = Path.Combine(_UserModel.Home, ESql.DDL_FILE);
+            //if (!File.Exists(path))
+            //{
+            //    return;
+            //}
 
-            StreamReader reader = File.OpenText(path);
-            XmlDocument doc = new XmlDocument();
-            doc.Load(reader);
-            reader.Close();
+            //StreamReader reader = File.OpenText(path);
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(reader);
+            //reader.Close();
 
-            LoadLib(doc);
+            //LoadLib(doc);
 
-            _Drives = new List<Rdbms>();
-            foreach (XmlNode node in doc.SelectNodes("/Amon/RDBMS"))
-            {
-                Rdbms dbms = new Rdbms();
-                dbms.FromXml(node);
-                _Drives.Add(dbms);
-            }
+            //_Drives = new List<Rdbms>();
+            //foreach (XmlNode node in doc.SelectNodes("/Amon/RDBMS"))
+            //{
+            //    Rdbms dbms = new Rdbms();
+            //    dbms.FromXml(node);
+            //    _Drives.Add(dbms);
+            //}
+
+            ChangeDdl(null);
         }
 
         private void ASql_KeyDown(object sender, KeyEventArgs e)
@@ -145,16 +276,24 @@ namespace Me.Amon.Sql
 
         private void PbMenu_Click(object sender, EventArgs e)
         {
+            CmMenu.Show(PbMenu, 0, PbMenu.Height);
         }
 
-        private void TcParams_SelectedIndexChanged(object sender, EventArgs e)
+        private void TcEditor_SelectedIndexChanged(object sender, EventArgs e)
         {
+            TabPage page = TcEditor.SelectedTab;
+            _DbEditor = page.Controls[0] as IEditor;
+        }
+
+        private void TcEditor_TabClosing(object sender, TabControlCancelEventArgs e)
+        {
+            e.Cancel = TcEditor.TabCount < 2;
         }
 
         private void TcResult_SelectedIndexChanged(object sender, EventArgs e)
         {
             TabPage page = TcResult.SelectedTab;
-            _DataGrid = page.Controls[0] as DbResult;
+            _DbResult = page.Controls[0] as DbResult;
         }
 
         private void TcResult_TabClosing(object sender, TabControlCancelEventArgs e)
@@ -169,32 +308,32 @@ namespace Me.Amon.Sql
         #endregion
 
         #region 私有函数
-        private void DoExecute()
+        public void DoExecute()
         {
-            if (!_Param.Check())
+            if (!_DbEditor.Check())
             {
+                ShowEcho("请输入或选择您要执行的语句！");
                 return;
             }
 
-            string sql = _Param.GetSQL().Trim();
-            IDbCommand command = GetCommand();
-            command.CommandText = sql;
-            if (!sql.ToLower().StartsWith("select"))
+            try
             {
-                _DataGrid.DataSource = null;
-                int row = command.ExecuteNonQuery();
-                ShowEcho(string.Format("共影响 {0} 条数据", row));
-                return;
-            }
+                string sql = _DbEditor.GetSQL().Trim();
+                if (!sql.ToLower().StartsWith("select"))
+                {
+                    int row = _SqlEngine.DoExecute(sql);
+                    ShowEcho(string.Format("共影响 {0} 条数据", row));
+                    return;
+                }
 
-            DataSet dataSet = new DataSet();
-            IDbDataAdapter adapter = GetAdapter();
-            adapter.SelectCommand = command;
-            adapter.Fill(dataSet);
-            if (dataSet.Tables.Count > 0)
+                DataSet dataSet = _SqlEngine.DoSelect(sql);
+                DataTable table = dataSet.Tables[0];
+                _DbResult.DataSource = table;
+                ShowEcho(string.Format("共查询到 {0} 条数据！", table.Rows.Count));
+            }
+            catch (Exception exp)
             {
-                _DataGrid.DataSource = dataSet.Tables[0];
-                ShowEcho(string.Format("查询结果共 {0} 条", dataSet.Tables[0].Rows.Count));
+                Main.ShowError(exp);
             }
         }
 
@@ -205,129 +344,47 @@ namespace Me.Amon.Sql
 
         private bool LoadLib(XmlDocument doc)
         {
-            _Assemblys = new Dictionary<string, Assembly>();
-            _Assemblys[""] = Assembly.GetExecutingAssembly();
+            //_Assemblys = new Dictionary<string, Assembly>();
+            //_Assemblys[""] = Assembly.GetExecutingAssembly();
 
-            XmlNodeList list = doc.SelectNodes("/Amon/Libs/Lib");
-            if (list == null || list.Count < 1)
-            {
-                return true;
-            }
+            //XmlNodeList list = doc.SelectNodes("/Amon/Libs/Lib");
+            //if (list == null || list.Count < 1)
+            //{
+            //    return true;
+            //}
 
-            try
-            {
-                string path = Application.StartupPath;
-                foreach (XmlNode node in list)
-                {
-                    string file = Dml.Attribute(node, "Path", "");
-                    if (!CharUtil.IsValidate(file))
-                    {
-                        continue;
-                    }
-                    if (!Path.IsPathRooted(file))
-                    {
-                        file = Path.Combine(path, file);
-                    }
-                    if (!File.Exists(file))
-                    {
-                        return false;
-                    }
-                    //动态加载程序集
-                    Assembly assembly = Assembly.LoadFrom(file);
-                    string id = Dml.Attribute(node, "Id", null);
-                    if (CharUtil.IsValidate(id))
-                    {
-                        _Assemblys[id] = assembly;
-                    }
-                }
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-        #endregion
-
-        #region 数据库操作
-        public IDbConnection GetConnection()
-        {
-            if (_Connect != null)
-            {
-                return _Connect;
-            }
-
-            if ("System.Data.OleDb.OleDbConnection" == _Dbms.ConnectionClass)
-            {
-                _Connect = new System.Data.OleDb.OleDbConnection();
-            }
-            else
-            {
-                Assembly assembly = _Assemblys[_Dbms.LibId];
-                _Connect = assembly.CreateInstance(_Dbms.ConnectionClass) as IDbConnection;
-            }
-            if (_Connect != null)
-            {
-                _Connect.ConnectionString = string.Format(_Dbms.ConnectionString, _Dbms.Uri, _Dbms.User, _Dbms.Password);
-                _Connect.Open();
-            }
-            return _Connect;
-        }
-
-        public IDbCommand GetCommand()
-        {
-            if (_Command != null)
-            {
-                return _Command;
-            }
-
-            if ("System.Data.OleDb.OleDbCommand" == _Dbms.CommandClass)
-            {
-                _Command = new System.Data.OleDb.OleDbCommand();
-            }
-            else
-            {
-                Assembly assembly = _Assemblys[_Dbms.LibId];
-                _Command = assembly.CreateInstance(_Dbms.CommandClass) as IDbCommand;
-            }
-            if (_Command != null)
-            {
-                _Command.Connection = GetConnection();
-                _Command.CommandType = CommandType.Text;
-            }
-            return _Command;
-        }
-
-        private IDbDataAdapter GetAdapter()
-        {
-            if (_Adapter != null)
-            {
-                return _Adapter;
-            }
-
-            if ("System.Data.OleDb.OleDbDataAdapter" == _Dbms.AdapterClass)
-            {
-                _Adapter = new System.Data.OleDb.OleDbDataAdapter();
-            }
-            else
-            {
-                Assembly assembly = _Assemblys[_Dbms.LibId];
-                _Adapter = assembly.CreateInstance(_Dbms.AdapterClass) as IDbDataAdapter;
-            }
-            return _Adapter;
-        }
-
-        private void CloseConnection()
-        {
-            if (_Command != null)
-            {
-                _Command.Dispose();
-            }
-            if (_Connect != null)
-            {
-                _Connect.Close();
-            }
-            _Adapter = null;
+            //try
+            //{
+            //    string path = Application.StartupPath;
+            //    foreach (XmlNode node in list)
+            //    {
+            //        string file = Dml.Attribute(node, "Path", "");
+            //        if (!CharUtil.IsValidate(file))
+            //        {
+            //            continue;
+            //        }
+            //        if (!Path.IsPathRooted(file))
+            //        {
+            //            file = Path.Combine(path, file);
+            //        }
+            //        if (!File.Exists(file))
+            //        {
+            //            return false;
+            //        }
+            //        //动态加载程序集
+            //        Assembly assembly = Assembly.LoadFrom(file);
+            //        string id = Dml.Attribute(node, "Id", null);
+            //        if (CharUtil.IsValidate(id))
+            //        {
+            //            _Assemblys[id] = assembly;
+            //        }
+            //    }
+            return true;
+            //}
+            //catch (Exception)
+            //{
+            //    return false;
+            //}
         }
         #endregion
     }
