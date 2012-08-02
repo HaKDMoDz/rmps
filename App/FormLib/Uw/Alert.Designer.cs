e0b61c97c898c6bd448aa4e9d36993c10d53469d
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-08-02 14:28:26 +0000

    阿木密码箱 V7.1.6.13 App导航功能。

---
+++ App/FormLib/Uw/Alert.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Me.Amon.Uw
+{
+    partial class Alert
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PbIcon = new System.Windows.Forms.PictureBox();
+            this.TbInfo = new System.Windows.Forms.TextBox();
+            this.BtOk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PbIcon
+            // 
+            this.PbIcon.Image = global::Me.Amon.Properties.Resources.Alert;
+            this.PbIcon.Location = new System.Drawing.Point(12, 12);
+            this.PbIcon.Name = "PbIcon";
+            this.PbIcon.Size = new System.Drawing.Size(32, 32);
+            this.PbIcon.TabIndex = 0;
+            this.PbIcon.TabStop = false;
+            // 
+            // TbInfo
+            // 
+            this.TbInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TbInfo.Location = new System.Drawing.Point(50, 25);
+            this.TbInfo.Multiline = true;
+            this.TbInfo.Name = "TbInfo";
+            this.TbInfo.ReadOnly = true;
+            this.TbInfo.Size = new System.Drawing.Size(212, 46);
+            this.TbInfo.TabIndex = 1;
+            this.TbInfo.TabStop = false;
+            // 
+            // BtOk
+            // 
+            this.BtOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtOk.Location = new System.Drawing.Point(187, 77);
+            this.BtOk.Name = "BtOk";
+            this.BtOk.Size = new System.Drawing.Size(75, 23);
+            this.BtOk.TabIndex = 2;
+            this.BtOk.Text = "确定(&O)";
+            this.BtOk.UseVisualStyleBackColor = true;
+            this.BtOk.Click += new System.EventHandler(this.BtOk_Click);
+            // 
+            // Alert
+            // 
+            this.AcceptButton = this.BtOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtOk;
+            this.ClientSize = new System.Drawing.Size(274, 112);
+            this.Controls.Add(this.BtOk);
+            this.Controls.Add(this.TbInfo);
+            this.Controls.Add(this.PbIcon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Alert";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "提示";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Alert_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.PbIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PbIcon;
+        private System.Windows.Forms.TextBox TbInfo;
+        private System.Windows.Forms.Button BtOk;
+    }
+}
