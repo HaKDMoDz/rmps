a34453aaf8655bca04263c24b22b5d565bf445ef
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-08-06 14:27:02 +0000

    阿木密码箱 V7.1.6.13 App导航功能。

---
+++ Web/ALot/V/Uc1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+
+namespace Me.Amon.Lot.V
+{
+    public partial class Uc1 : UserControl
+    {
+        public Uc1()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                textBox1.Text = value;
+            }
+        }
+    }
+}
