9308533fb386b22775d52e5700841023ba5ca59f
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-08-22 23:03:43 +0000

    阿木密码箱 V7.1.6.13 App导航功能。

---
+++ App/FormLib/Uc/KeyStroke.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Me.Amon.Uc
+{
+    public class KeyStroke<T>
+    {
+        public bool Control { get; set; }
+        public bool Alt { get; set; }
+        public bool Shift { get; set; }
+        public bool Meta { get; set; }
+        public Keys Code { get; set; }
+        public string Key { get; set; }
+        public string Memo { get; set; }
+        public string Command { get; set; }
+        public IAction<T> Action { get; set; }
+
+        public bool Decode(string key)
+        {
+            Key = Regex.Replace(key, "[^-+=`;',./\\[\\]a-zA-Z0-9]+", " ").Trim();
+            key = Key.ToUpper();
+            foreach (string t in key.Split(' '))
+            {
+                if (string.IsNullOrWhiteSpace(t))
+                {
+                    continue;
+                }
+                if ("CTRL" == t)
+                {
+                    Control = true;
+                    continue;
+                }
+                if ("ALT" == t)
+                {
+                    Alt = true;
+                    continue;
+                }
+                if ("SHIFT" == t)
+                {
+                    Shift = true;
+                    continue;
+                }
+                if ("META" == t)
+                {
+                    Meta = true;
+                    continue;
+                }
+                key = t;
+            }
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                return false;
+            }
+            Code = (Keys)Enum.Parse(typeof(Keys), key, true);
+            return Code != Keys.None;
+        }
+
+        public override string ToString()
+        {
+            //StringBuilder buffer = new StringBuilder();
+            //if (Control)
+            //{
+            //    buffer.Append(Keys.Control.ToString()).Append(' ');
+            //}
+            //if (Shift)
+            //{
+            //    buffer.Append(Keys.Shift.ToString()).Append(' ');
+            //}
+            //if (Alt)
+            //{
+            //    buffer.Append(Keys.Alt.ToString()).Append(' ');
+            //}
+            //buffer.Append(Code.ToString());
+            //return buffer.ToString();
+            return Key;
+        }
+    }
+}
