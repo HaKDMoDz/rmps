2b11dff98e4688d143dfcd350b85bcd99da8a408
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-08-24 12:57:49 +0000

    阿木密码箱 V7.1.6.13 App导航功能。

---
+++ App/FormLib/Uc/Tips.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace Me.Amon.Uc
+{
+    public partial class Tips : UserControl
+    {
+        public Tips()
+        {
+            InitializeComponent();
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return LlTips.Text;
+            }
+            set
+            {
+                LlTips.Text = value;
+            }
+        }
+
+        private void PbHide_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
