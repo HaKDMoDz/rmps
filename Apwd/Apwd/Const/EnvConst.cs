f351bf7a2d1fcde8dab3d3254e22ca0d144663fc
amon.hm <amon.hm@27759140-d95f-11de-8d45-899910d1343a>
2012-09-30 02:31:48 +0000

    系统清理

--- Apwd/Apwd/Const/EnvConst.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Me.Amon.Apwd.Const
-{
-    public class EnvConst
-    {
-        public const string SERVER_PATH = "http://localhost/s.php";
-        //public const string SERVER_PATH = "http://mpwd.sinaapp.com/s.php";
-    }
-}
