2f73266cc0176039190c8702d623e675dc7d943b
avenyao@gmail.com <avenyao@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-11-07 02:19:21 +0000

    本地管理

---
+++ App/Amon/Pcs/E/File/NativeAction.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Me.Amon.Pcs.E.File
+{
+    public class NativeAction : APcsAction
+    {
+        public override void EventHandler(object sender, EventArgs e)
+        {
+            if (IApp != null)
+            {
+                IApp.NewNative();
+            }
+        }
+    }
+}
