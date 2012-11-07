5eb0636c95c726725ebb593f2204cd38ec20bc83
avenyao@gmail.com <avenyao@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-11-07 09:34:42 +0000

    常量调整

--- App/Amon/Pcs/CPcs.cs
+++ App/Amon/Pcs/CPcs.cs
@@ -1,15 +1,18 @@
- namespace Me.Amon.Pcs
+namespace Me.Amon.Pcs
 {
     public class CPcs
     {
         public const int META_TYPE_UNKNOWN = 0;
         public const int META_TYPE_FOLDER = 1;
         public const int META_TYPE_FILE = 2;
-        
+
+        public const string PATH_FAVORITES = "?fav";
+        public const string PATH_LIBRARIES = "?lib";
         public const string PATH_DOCUMENTS = "?documents";
         public const string PATH_PICTURES = "?pictures";
         public const string PATH_AUDIOS = "?audios";
         public const string PATH_VIDEOS = "?videos";
+        public const string PATH_STORAGE = "?storage";
         public const string PATH_SHARES = "?shares";
         public const string PATH_RECYCLE = "?recycle";
 
