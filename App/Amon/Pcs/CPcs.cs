c27aeb25ecfd66cbc8a1896e71c03296d3198009
avenyao@gmail.com <avenyao@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-11-07 06:07:32 +0000

    关键词调整。

--- App/Amon/Pcs/CPcs.cs
+++ App/Amon/Pcs/CPcs.cs
@@ -6,12 +6,12 @@
         public const int META_TYPE_FOLDER = 1;
         public const int META_TYPE_FILE = 2;
         
-        public const string PATH_DOCUMENTS = "<documents>";
-        public const string PATH_PICTURES = "<pictures>";
-        public const string PATH_AUDIOS = "<audios>";
-        public const string PATH_VIDEOS = "<videos>";
-        public const string PATH_SHARES = "<shares>";
-        public const string PATH_RECYCLE = "<recycle>";
+        public const string PATH_DOCUMENTS = "?documents";
+        public const string PATH_PICTURES = "?pictures";
+        public const string PATH_AUDIOS = "?audios";
+        public const string PATH_VIDEOS = "?videos";
+        public const string PATH_SHARES = "?shares";
+        public const string PATH_RECYCLE = "?recycle";
 
         public const string XML_MENU = "WPcs.xml";
     }
