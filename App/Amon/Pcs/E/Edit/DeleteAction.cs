335eaec46689a1d135277fd90e9cc1763470a3dc
avenyao@gmail.com <avenyao@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-11-07 09:39:05 +0000

    delete

---
+++ App/Amon/Pcs/E/Edit/DeleteAction.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Me.Amon.Pcs.E.Edit
+{
+    public class DeleteAction : APcsAction
+    {
+        public override void EventHandler(object sender, EventArgs e)
+        {
+            if (IApp != null)
+            {
+                IApp.DeleteMeta();
+            }
+        }
+    }
+}
