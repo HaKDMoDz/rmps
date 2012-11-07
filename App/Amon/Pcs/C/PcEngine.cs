2e2db45484fba9fb2b92f4581f3d37716d391e8c
avenyao@gmail.com <avenyao@gmail.com@27759140-d95f-11de-8d45-899910d1343a>
2012-11-07 09:41:30 +0000

    Engine

--- App/Amon/Pcs/C/PcEngine.cs
+++ App/Amon/Pcs/C/PcEngine.cs
@@ -1,18 +1,46 @@
-﻿using System.IO;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using Me.Amon.Da;
+using Me.Amon.Da.Df;
 
 namespace Me.Amon.Pcs.C
 {
     public class PcEngine
     {
+        private DFA _DFA;
+        private string _Root;
+
+        public PcEngine()
+        {
+            _DFA = new DFEngine();
+        }
+
+        public void Init(string root)
+        {
+            _Root = root;
+            _DFA.Load(Path.Combine(root, "amon.sync"));
+        }
+
+        public int CompareVersion(string file, string ver)
+        {
+            return _DFA.Get(file, "").CompareTo(ver);
+        }
+
+        public void ChangeVergion(string file, string ver)
+        {
+            _DFA.Set(file, ver);
+        }
+
         public FileStream OpenWrite(string path)
         {
+            path = Path.Combine(_Root, path);
             return File.OpenWrite(path);
         }
 
         public FileStream OpenRead(string file)
         {
+            file = Path.Combine(_Root, file);
             return File.OpenRead(file);
         }
 
